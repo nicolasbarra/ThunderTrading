16033de855731aa699dc95b40faca4f0eaf68f78
Akhil Venkatasai Chandra <akhilvenkatasaic@gmail.com>
2020-06-09 22:41:37 -0500

    Updating dataframe from websocket

--- AlgoBegin.py
+++ AlgoBegin.py
@@ -3,11 +3,30 @@ import json
 import requests
 import websocket
 import os
+import pandas as pd
+import datetime
 
 socket = "wss://alpaca.socket.polygon.io/stocks"
 TICKERS = "AM.MSFT"
 API_KEY = os.getenv('APCA_API_KEY_ID')
 # HEADERS = {'APCA_API_KEY_ID': os.getenv("APCA_API_KEY_ID"), 'APCA_API_SECRET_KEY': os.getenv("APCA_API_SECRET_KEY")}
+
+
+# "ev": "AM",             // Event Type ( A = Second Agg, AM = Minute Agg )
+#     "sym": "MSFT",          // Symbol Ticker
+#     "v": 10204,             // Tick Volume
+#     "av": 200304,           // Accumulated Volume ( Today )
+#     "op": 114.04,           // Todays official opening price
+#     "vw": 114.4040,         // VWAP (Volume Weighted Average Price)
+#     "o": 114.11,            // Tick Open Price
+#     "c": 114.14,            // Tick Close Price
+#     "h": 114.19,            // Tick High Price
+#     "l": 114.09,            // Tick Low Price
+#     "a": 114.1314,          // Tick Average / VWAP Price
+#     "s": 1536036818784,     // Tick Start Timestamp ( Unix MS )
+#     "e": 1536036818784,     // Tick End Timestamp ( Unix MS )
+stocks = ["MSFT", "APPL", "GOOG"]
+df = pd.DataFrame(columns=(['Time'] + stocks))
 
 
 def on_open(webs):
@@ -26,7 +45,17 @@ def on_open(webs):
 
 def on_message(webs, message):
     current_line = json.loads(message)
-    print(current_line)
+    time = current_line["time"]
+    ticker = current_line["sym"]
+    price = current_line["vw"]
+    last_time = df['time'].iloc[-1]
+    if last_time != time:
+            df.append({"Time": time, ticker: price})
+            if df.size < 60:
+                df.drop(df.index[:1], inplace=True)
+    else:
+        df.loc[-1, ticker] = price
+    print(df)
 
 
 def on_close(webs):
