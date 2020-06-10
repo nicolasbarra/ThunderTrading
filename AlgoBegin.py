85944742481cb75b1065cd266a9def694a06ffe0
Akhil Venkatasai Chandra <akhilvenkatasaic@gmail.com>
2020-06-09 20:47:55 -0500

    Add websocket connectivity

--- AlgoBegin.py
+++ AlgoBegin.py
@@ -1,10 +1,37 @@
 import alpaca_trade_api as tradeapi
+import json
+import requests
+import websocket
+import os
 
-api = tradeapi.REST()
+socket = "wss://alpaca.socket.polygon.io/stocks"
+TICKERS = "AM.MSFT"
+API_KEY = os.getenv('APCA_API_KEY_ID')
+# HEADERS = {'APCA_API_KEY_ID': os.getenv("APCA_API_KEY_ID"), 'APCA_API_SECRET_KEY': os.getenv("APCA_API_SECRET_KEY")}
 
-aapl = api.polygon.historic_agg_v2('AAPL', 1, 'day', _from='2019-01-01', to='2019-02-01').df
 
-# aapl = api.polygon.daily_open_close('AAPL', '2020-06-01')
+def on_open(webs):
+    auth_data = {
+        "action": "auth",
+        "params": API_KEY
+    }
+    webs.send(json.dumps(auth_data))
+    channel_data = {
+        "action": "subscribe",
+        "params": TICKERS
+    }
+    print("Connection opened.")
+    webs.send(json.dumps(channel_data))
 
-print(aapl)
 
+def on_message(webs, message):
+    current_line = json.loads(message)
+    print(current_line)
+
+
+def on_close(webs):
+    print("Connection closed.")
+
+
+ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
+ws.run_forever()
